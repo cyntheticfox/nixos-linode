1278393795f623c9a77d292e8a8f018612871291
David Houston <houstdav000@gmail.com>
2022-01-14 07:52:38 -0500

    Add Compressed Output
    Linode requires any uploaded images to be gzip-compressed, so create a
    derivation providing such and make it the default package.

--- flake.nix
+++ flake.nix
@@ -13,16 +13,44 @@
   outputs = { self, ... }@inputs:
   with inputs;
   {
-    packages.x86_64-linux.linode = nixos-generators.nixosGenerate {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      modules = [
-        nixpkgs.nixosModules.notDetected
-        ./configuration.nix
-        ./hardware-configuration.nix
-      ];
-      format = "raw";
+    packages.x86_64-linux = {
+      linode-img = nixos-generators.nixosGenerate {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          nixpkgs.nixosModules.notDetected
+          ./configuration.nix
+          ./hardware-configuration.nix
+        ];
+        format = "raw";
+      };
+
+      linode-compressed =
+      let
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      in
+        pkgs.stdenvNoCC.mkDerivation {
+          name = "linode-compressed-image.img.gz";
+
+          src = self.packages.x86_64-linux.linode-img;
+
+          unpackPhase = ''
+            # Copy over source file
+            cp $src/nixos.img ./nixos.img
+          '';
+
+          # gzip is thankfully provided by stdenv
+          buildPhase = ''
+            # Compress with gzip
+            gzip ./nixos.img
+          '';
+
+          installPhase = ''
+            # Copy produced file
+            cp ./nixos.img.gz $out
+          '';
+        };
     };
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.linode;
+    defaultPackage.x86_64-linux = self.packages.x86_64-linux.linode-compressed;
   };
 }
